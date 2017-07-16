1f49309d57d3ad9f3ab02f354f609071baa4c1d6
dzm <vvv-rrrr@list.ru>
2017-07-16 15:49:53 +0300

    code

---
+++ Customer/Customer/Customer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Customers
+{
+    public class Customer:IFormattable
+    {
+        /// <summary>
+        /// properties
+        /// </summary>
+        public string Name { get; private set; }
+        public string Phone { get; private set; }
+        public decimal Revenue { get; private set; }
+        /// <summary>
+        /// c-or
+        /// </summary>
+        /// <param name="name">name</param>
+        /// <param name="phone">number phone</param>
+        /// <param name="revenue">money</param>
+        public Customer(string name, string phone, decimal revenue)
+        {
+            if (name == null || phone == null) throw new ArgumentNullException();
+            Phone = phone;
+            Name = name;
+            Revenue = revenue;
+        }
+        /// <summary>
+        /// return string format
+        /// </summary>
+        /// <returns>string format</returns>
+        public override string ToString()
+        {
+            return ToString("G", CultureInfo.CurrentCulture);
+        }
+        /// <summary>
+        /// return string format
+        /// </summary>
+        /// <param name="format">format</param>
+        /// <returns>string format</returns>
+        public string ToString(string format)
+        {
+            return ToString(format, CultureInfo.CurrentCulture);
+        }
+        /// <summary>
+        /// return string format
+        /// </summary>
+        /// <param name="format">format</param>
+        /// <param name="provider">provider format</param>
+        /// <returns>string format</returns>
+        public string ToString(string format, IFormatProvider provider)
+        {
+            if (String.IsNullOrEmpty(format)) format = "G";
+            if (provider == null) provider = CultureInfo.CurrentCulture;
+
+            switch (format.ToUpperInvariant())
+            {
+                case "G":
+                    return String.Format("{0},{1}, +# (###) #### ", $"Customer record: nameof{Name}, nameof{Revenue}, nameof{Phone}");
+                case "P":
+                    return String.Format(" +# (###) ####", $"Customer record: nameof{Phone}");
+                case "NR":
+                    return $"Customer record: nameof{Name}, nameof{Revenue}";
+                case "N":
+                    return $"Customer record: nameof{Name}";
+                case "R":
+                    return $"Customer record: nameof{Revenue}";
+
+                default:
+                    throw new FormatException(String.Format("The {0} format string is not supported.", format));
+            }
+        }
+
+    }
+}
---
+++ Customer/CustomerFormatProvider/CustomerFormatProvider.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Customers;
+
+namespace CustomerFormatProviders
+{
+    public class CustomerFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        /// <summary>
+        /// GetFormat return referenes on object or null
+        /// </summary>
+        /// <param name="formatType">Type</param>
+        /// <returns>object</returns>
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+                return this;
+            else
+                return null;
+        }
+        /// <summary>
+        /// return object in format string
+        /// </summary>
+        /// <param name="format">format/param>
+        /// <param name="obj">object</param>
+        /// <param name="formatProvider">IFormatProvider</param>
+        /// <returns>string format</returns>
+        public string Format(string format, object obj, IFormatProvider formatProvider)
+        {
+            if (obj.GetType() != typeof(Customer) || obj == null) 
+                {
+                    return HandleOtherFormats(format, obj);
+                }
+            Customer customer = obj as Customer;
+            if (customer == null) HandleOtherFormats(format, obj);
+            switch (format.ToUpperInvariant())
+            {
+                case "A":
+                    return $"Name is {customer.Name}. Revenue:{customer.Revenue}, Phone{customer.Phone}";
+                case "HTML":
+                    return $"<html><head></head><body><p>Name is {customer.Name}. Revenue:{customer.Revenue}, Phone{customer.Phone}</p></body>";
+                default:
+                    throw new FormatException(String.Format("The {0} format string is not supported.", format));
+            }
+        }
+        /// <summary>
+        /// Handle other Formats
+        /// </summary>
+        /// <param name="format">string</param>
+        /// <param name="arg">object</param>
+        /// <returns>string</returns>
+        private string HandleOtherFormats(string format, object arg)
+        {
+            if (arg is IFormattable)
+                return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+            else if (arg != null)
+                return arg.ToString();
+            else
+                return String.Empty;
+        }
+    }
+}
